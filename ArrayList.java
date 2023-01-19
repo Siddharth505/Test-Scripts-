509d346453a77c5b627606097581d27e4eab87ac
Siddharth <71707432+Siddharth505@users.noreply.github.com>
2023-01-19 16:35:06 +0530

    Add files via upload

---
+++ ArrayList.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import org.testng.annotations.Test;
+
+public class ArrayList 
+{
+	@Test
+	public void A()
+	{
+		java.util.ArrayList al = new java.util.ArrayList();
+		
+		al.add("Siddharth");
+		al.add(10);
+		al.add(null);
+		al.add(3, 42.32);
+		System.out.println(al);
+	}
+
+}
